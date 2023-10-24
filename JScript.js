ab13b7983421abf9b9f95427deebe2f9513a0a58
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-24 18:56:25 +0100

    Update JScript.js

--- JScript.js
+++ JScript.js
@@ -25,3 +25,22 @@ for (i = 0; i < coll.length; i++) {
   });
 
 }
+// Add a keyboard event listener to each button
+collapsibleButtons.forEach(button => {
+    button.addEventListener('keydown', event => {
+      // If the user presses the Enter key or the Space key
+      if (event.code === 'Enter' || event.code === 'Space') {
+        // Prevent the default action of the button
+        event.preventDefault();
+        // Toggle the active class on the button
+        button.classList.toggle('active');
+        // Get the content of the collapsible box
+        const content = button.nextElementSibling;
+        // Toggle the display of the content
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+        } else {
+          content.style.display = 'block';
+        }
+      }
+    });
