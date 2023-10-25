872bbf53d08bac3770e7e4a6c2cd9472cb5e82ab
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-25 19:43:14 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,32 @@
+var collapsibleButtons = document.getElementsByClassName("Collapsible-button");
 
+for (var i = 0; i < collapsibleButtons.length; i++) {
+  collapsibleButtons[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var faqs = this.nextElementSibling;
+    if (faqs.style.display === "block") {
+      faqs.style.display = "none";
+    } else {
+      faqs.style.display = "block";
+    }
+  });
+  
+  // Add a keyboard event listener to each button
+  collapsibleButtons[i].addEventListener('keydown', event => {
+    // If the user presses the Enter key or the Space key
+    if (event.code === 'Enter' || event.code === 'Space') {
+      // Prevent the default action of the button
+      event.preventDefault();
+      // Toggle the active class on the button
+      collapsibleButtons[i].classList.toggle('active');
+      // Get the content of the collapsible box
+      const content = collapsibleButtons[i].nextElementSibling;
+      // Toggle the display of the content
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    }
+  });
+}
