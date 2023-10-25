d9d038457e59235fe4a78f34bda9fce98e3c62af
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-25 22:12:23 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,24 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
       // Toggle the display of the corresponding FAQ element
-      faqs[index].style.display = faqs[index].style.display === 'block' ? 'none' : 'block';
-      // Toggle the active class on the button
-      button.classList.toggle('active');
-    });
-  });
-
-  // Add a keyboard event listener to each button
-  buttons.forEach((button, index) => {
-    button.addEventListener('keydown', event => {
-      // If the user presses the Enter key or the Space key
-      if (event.code === 'Enter' || event.code === 'Space') {
-        // Prevent the default action of the button
-        event.preventDefault();
-        // Toggle the display of the corresponding FAQ element
-        faqs[index].style.display = faqs[index].style.display === 'block' ? 'none' : 'block';
-        // Toggle the active class on the button
-        button.classList.toggle('active');
-      }
+      faqs[index].classList.toggle('active');
     });
   });
 });
