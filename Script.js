ce879867ddfc4b397151eeca44070b4fe9bc7093
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-25 21:33:52 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,28 +1,31 @@
-// Get the collapsible button and the FAQs element
-const collapsibleButton = document.querySelector('.Collapsible-button');
+// Get all collapsible buttons and the FAQs element
+const collapsibleButtons = document.querySelectorAll('.Collapsible-button');
 const faqs = document.querySelector('.faqs');
 
-// Add a click listener to the collapsible button
-collapsibleButton.addEventListener('click', () => {
-  // Toggle the display of the FAQs element
-  faqs.style.display = faqs.style.display === 'block' ? 'none' : 'block';
-});
+// Loop through each button and add event listeners
+for (let i = 0; i < collapsibleButtons.length; i++) {
+  // Add a click listener to the button
+  collapsibleButtons[i].addEventListener('click', () => {
+    // Toggle the display of the FAQs element
+    faqs.style.display = faqs.style.display === 'block' ? 'none' : 'block';
+  });
 
-// Add a keyboard event listener to the button
-collapsibleButton.addEventListener('keydown', event => {
-  // If the user presses the Enter key or the Space key
-  if (event.code === 'Enter' || event.code === 'Space') {
-    // Prevent the default action of the button
-    event.preventDefault();
-    // Toggle the active class on the button
-    event.target.classList.toggle('active');
-    // Get the content of the collapsible box
-    const content = event.target.nextElementSibling;
-    // Toggle the display of the content
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
-    } else {
-      content.style.display = 'block';
+  // Add a keyboard event listener to the button
+  collapsibleButtons[i].addEventListener('keydown', event => {
+    // If the user presses the Enter key or the Space key
+    if (event.code === 'Enter' || event.code === 'Space') {
+      // Prevent the default action of the button
+      event.preventDefault();
+      // Toggle the active class on the button
+      event.target.classList.toggle('active');
+      // Get the content of the collapsible box
+      const content = event.target.nextElementSibling;
+      // Toggle the display of the content
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
     }
-  }
-});
+  });
+}
