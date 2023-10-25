2b3a6e3f538d9b6c87755fec7e2ffd972ef782aa
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-25 22:18:03 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,32 @@
-const collapsibleButtons = document.querySelectorAll('.Collapsible-button');
+var collapsibleButtons = document.getElementsByClassName("Collapsible-button");
 
-collapsibleButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const faqSection = button.nextElementSibling;
-    faqSection.style.display = faqSection.style.display === 'block' ? 'none' : 'block';
+for (var i = 0; i < collapsibleButtons.length; i++) {
+  collapsibleButtons[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var faqs = this.nextElementSibling;
+    if (faqs.style.display === "block") {
+      faqs.style.display = "none";
+    } else {
+      faqs.style.display = "block";
+    }
   });
-});
+
+  // Add a keyboard event listener to each button
+  collapsibleButtons[i].addEventListener('keydown', event => {
+    // If the user presses the Enter key or the Space key
+    if (event.code === 'Enter' || event.code === 'Space') {
+      // Prevent the default action of the button
+      event.preventDefault();
+      // Toggle the active class on the button
+      collapsibleButtons[i].classList.toggle('active');
+      // Get the content of the collapsible box
+      const content = collapsibleButtons[i].nextElementSibling;
+      // Toggle the display of the content
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    }
+  });
+}
