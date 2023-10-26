0a138bef66d394d264697e9d416e7a84023f556f
RobertLawren <140003565+RobertLawren@users.noreply.github.com>
2023-10-26 11:56:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,9 @@
+window.addEventListener('scroll', function() {
+  if (document.documentElement.scrollTop >= 300) {
+    document.documentElement.style.zoom = 100;
+  }
+});
+
 var collapsibleButtons = document.getElementsByClassName("Collapsible-button");
 
 for (var i = 0; i < collapsibleButtons.length; i++) {
